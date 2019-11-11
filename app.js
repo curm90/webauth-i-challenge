a128f7ded6db3bf9128ed9f71357228348bd938d
Liam Sutton <devliam90@gmail.com>
2019-11-11 14:04:56 +0000

    add session config object

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 const Users = require('./src/users/user-model');
 
 const app = express();
 
+const sessionConfig = {
+  name: 'mysession',
+  secret: 'nobody tosses a dwarf!',
+  cookie: {
+    maxAge: 1000 * 30,
+    secure: false
+  },
+  httpOnly: true,
+  resave: false,
+  saveOnInitialized: false
+};
+
 app.use(helmet());
 app.use(express.json());
+app.use(sessionConfig);
 
 app.get('/', (req, res) => {
   res.send('Its alive');
@@ -24,6 +38,47 @@ app.post('/api/register', (req, res) => {
     .catch(error => {
       res.status(500).json({ message: error.message });
     });
+});
+
+app.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}` });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function restricted(req, res, next) {
+  const { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ messasge: 'Invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message });
+    });
+}
+
+app.get('/api/users', restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => res.send(error));
 });
 
 module.exports = app;
