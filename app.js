1a45555a53b52ad9a8867710bcb33c423c8e2d9e
Liam Sutton <devliam90@gmail.com>
2019-11-11 14:29:37 +0000

    add sessions

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const Users = require('./src/users/user-model');
 
 const app = express();
 
-const sessionConfig = {
-  name: 'mysession',
-  secret: 'nobody tosses a dwarf!',
-  cookie: {
-    maxAge: 1000 * 30,
-    secure: false
-  },
-  httpOnly: true,
-  resave: false,
-  saveOnInitialized: false
-};
-
 app.use(helmet());
 app.use(express.json());
-app.use(sessionConfig);
+app.use(
+  session({
+    name: 'mysession',
+    secret: process.env.SECRET,
+    cookie: {
+      maxAge: 1000 * 30,
+      secure: false
+    },
+    httpOnly: true,
+    resave: false,
+    saveOnInitialized: false
+  })
+);
 
 app.get('/', (req, res) => {
   res.send('Its alive');
@@ -47,6 +47,7 @@ app.post('/api/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}` });
       } else {
         res.status(401).json({ message: 'Invalid credentials' });
@@ -57,20 +58,28 @@ app.post('/api/login', (req, res) => {
     });
 });
 
+// function restricted(req, res, next) {
+//   const { username, password } = req.body;
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         next();
+//       } else {
+//         res.status(401).json({ messasge: 'Invalid credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json({ message: error.message });
+//     });
+// }
+
 function restricted(req, res, next) {
-  const { username, password } = req.body;
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({ messasge: 'Invalid credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ message: error.message });
-    });
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ message: 'You shall not pass' });
+  }
 }
 
 app.get('/api/users', restricted, (req, res) => {
